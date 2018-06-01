aec812096d6dc3509df135e1544cfb03fd8ce337
michael.meier <mikmeier@hotmail.com>
2018-06-01 14:19:07 +0200

    html and css

---
+++ src/js/data.js
@@ -0,0 +1,36 @@
+data =  {
+    initData: {
+        noteButton         : "Create new Note",
+        changeStyleDropdown: [
+            {
+                name : 'Simple-Style',
+                value: 1
+            },
+            {
+                name : 'Color-Style',
+                value: 2
+            }
+        ],
+        navButtonList      : [
+            {
+                name: 'By finish Date',
+            },
+            {
+                name: 'By created Date'
+            },
+            {
+                name: 'By Importance'
+            }
+        ],
+        finishedButton     : 'Show finished'
+    },
+
+    editData: {
+        title: 'Title'
+    },
+
+    notes:{
+
+    }
+}
+
